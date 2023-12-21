587ba9db47c02d9003b07ef30f0b7f700aea1845
SGVp15 <phosphorusp15@mail.ru>
2023-12-21 21:52:42 +0300

    config

--- Config/config_private.py
+++ Config/config_private.py
@@ -1,15 +1,16 @@
 from dotenv import dotenv_values
-from Config.manages_id_email import user_id_email
 
 config = dotenv_values('./Config/.env')
+
 BOT_TOKEN = config['BOT_TOKEN']
 
-ADMIN_ID = [int(x) for x in config['ADMIN_ID'].split(',')]
-USER_ID_EMAIL = user_id_email
+ADMIN_ID = list(dotenv_values('./Config/.env_telegram_admin').keys())
+
+user_id_email = dotenv_values('./Config/.env_manager_telegram_id_email')
 USERS_ID = list(user_id_email.keys())
 # ------------------------------------------------------------
 #  Webinar
-WEBINAR_TOKENS = [x.strip() for x in config['WEBINAR_TOKEN'].split(',')]
+WEBINAR_TOKENS = list(dotenv_values('./Config/.env_webinar_token'))
 # ------------------------------------------------------------
 # Email
 EMAIL_LOGIN = config['EMAIL_LOGIN']
--- Config/manages_id_email.py
+++
@@ -1,2 +0,0 @@
-user_id_email = {822072027: 'g.savushkin@itexpert.ru', 167572883: '', 263161488: 'a.rybalkin@itexpert.ru',
-                 5226450762: 'a.katkov@itexpert.ru'}
