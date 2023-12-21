e3e437f8acefd58bff075a52b68a0b8de39a9132
SGVp15 <phosphorusp15@mail.ru>
2023-12-21 22:23:55 +0300

    queue

--- handlers/log.py
+++ handlers/log.py
@@ -1,6 +1,6 @@
 from aiogram import types
 
-from Config.config import SELLERS, QUEUE, LOG_FILE
+from Config.config import SELLERS, LOG_FILE
 from Config.config_private import USERS_ID, ADMIN_ID
 from keybords.inline import inline_kb_main
 from loader import dp, bot
@@ -24,9 +24,7 @@ async def show_queue(callback_query: types.callback_query):
     await bot.send_message(chat_id=callback_query.from_user.id, text=get_queue(), reply_markup=inline_kb_main)
 
 
-@dp.callback_query_handler(
-    lambda c: c.data in ['get_log', 'get_seller', 'get_courses', 'get_history', 'get_log_program'],
-    user_id=[*ADMIN_ID, *USERS_ID])
+@dp.callback_query_handler(lambda c: c.data in ['get_log', 'get_seller'], user_id=[*ADMIN_ID, *USERS_ID])
 async def get_file(callback_query: types.callback_query):
     query = callback_query.data
     file = LOG_FILE
@@ -34,8 +32,6 @@ async def get_file(callback_query: types.callback_query):
         file = SELLERS
     elif query == 'get_log':
         file = LOG_FILE
-    elif query == 'get_queue':
-        file = QUEUE
 
     if is_empty_file(file):
         await bot.send_message(chat_id=callback_query.from_user.id, text=f'✅ Файл {file} пустой',
