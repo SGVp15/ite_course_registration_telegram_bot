e8d1e399ea1472b1679262036f7914d667d97905
SGVp15 <phosphorusp15@mail.ru>
2024-01-27 14:31:13 +0300

    aiogram3.

--- Telegram/handlers/menu.py
+++ Telegram/handlers/menu.py
@@ -14,15 +14,15 @@ async def zoom_menu(callback_query: types.callback_query):
                                         reply_markup=inline_kb_zoom)
 
 
-@dp.callback_query((F.data == callBackData.admin_menu) & F.from_user.id.in_({*ADMIN_ID}))
+@dp.callback_query((F.data == callBackData.admin_menu) & (F.from_user.id.in_({*ADMIN_ID})))
 async def admin_menu(callback_query: types.callback_query):
     await bot.edit_message_reply_markup(chat_id=callback_query.from_user.id,
                                         message_id=callback_query.message.message_id,
                                         reply_markup=inline_kb_admin)
 
 
-# @dp.callback_query(F.data.in_(callBackData.back_to_main_menu))
-@dp.callback_query(F.data == callBackData.back_to_main_menu)
+@dp.callback_query(F.data.in_(callBackData.back_to_main_menu))
+# @dp.callback_query(F.data == callBackData.back_to_main_menu)
 async def back_to_main(callback_query: types.callback_query):
     await bot.edit_message_reply_markup(chat_id=callback_query.from_user.id,
                                         message_id=callback_query.message.message_id,
