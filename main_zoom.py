66091b019219225e7c9746c0d93336b0859884bd
SGVp15 <phosphorusp15@mail.ru>
2024-01-30 13:18:46 +0300

    zoom

---
+++ main_zoom.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from Zoom.Clicker import run_clicker
+
+
+async def main():
+    tasks = [
+        run_clicker(),
+    ]
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    asyncio.run(main())
