5a977e90c9e82973f531858fda7d8ef1f31681b0
SGVp15 <phosphorusp15@mail.ru>
2024-01-30 13:19:12 +0300

    zoom

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ from Telegram.main import start_bot
 async def main():
     tasks = [
         start_bot(),
-        run_clicker(),
+        # run_clicker(),
     ]
     await asyncio.gather(*tasks)
-
-# async def main():
-#     task = asyncio.create_task(start_bot())
-#     task = asyncio.create_task(run_clicker())
-#     # await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
