f33e27717cbba151a9ce46d2ae6e84a9580217ed
SGVp15 <phosphorusp15@mail.ru>
2024-07-29 12:06:18 +0300

    regexp

--- Config/config.py
+++ Config/config.py
@@ -10,11 +10,16 @@ WEBINAR_LOG = './data/webinar_registration.txt'
 
 IMPORT_FILE = './data/input.txt'
 OLD_USERS = './data/users.txt'
+
 # -----------------------------------------------------------------------------------------------------------------------
-PATTERN_URL = r'https://[^\s]+'
-# 'https://events.webinar.ru/ITExpert/569285096/1bd05f2176c42f9208556acf5e594f32'
-PATTERN_WEBINAR_EVENT_ID = r'https://(my.mts-link.ru|events.webinar.ru)/\w+/(\d+)/*'
-PATTERN_ZOOM_URL = r'\s*(https://.*zoom.us/.*\S+)'
+# https://events.webinar.ru/ITExpert/569285096/1bd05f2176c42f9208556acf5e594f32
+# https://us06web.zoom.us/meeting/register/tZAscequpz0sGd0hMbssnWyDoB8nDJ4GeHfL
+
+PATTERN_URL = r'https://\S+'
+PATTERN_WEBINAR_EVENT_ID = r'https://(my.mts-link.ru|events.webinar.ru)/\w+/(\d+)'
+PATTERN_ZOOM_URL = r"\s*(https://.*zoom.us/\S+)"
+
 # -----------------------------------------------------------------------------------------------------------------------
+
 FILE_XPATH_BTN_ZOOM_REGISTRATION = './Config/xpath_btn_registration_zoom.txt'
 WEBINAR_HISTORY = './data/.webinar_history.txt'
