d38c63f51b4ff0e5f918c2b1a669eae76f9d78cb
SGVp15 <phosphorusp15@mail.ru>
2024-10-01 11:38:13 +0300

    git reset

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from selenium import webdriver
 import asyncio
 
+from selenium import webdriver
+
+from Telegram.main import start_bot
 from Utils.log import log
 from Zoom.Clicker import run_clicker
-from Telegram.main import start_bot
 
 
 async def main():
@@ -15,12 +16,11 @@ async def main():
 
 
 if __name__ == '__main__':
-    # driver = webdriver.Chrome()
-    # driver.close()
+    driver = webdriver.Chrome()
+    driver.close()
 
     try:
         log.info('course registration bot START')
         asyncio.run(main())
     finally:
         log.error('course registration bot STOP')
-# git pull https://github.com/SGVp15/course_registration_telegram_bot | python main.py
