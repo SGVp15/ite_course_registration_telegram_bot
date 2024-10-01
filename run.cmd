6eda1490b9015100180adedf8affa77ae21da884
SGVp15 <phosphorusp15@mail.ru>
2024-10-01 11:26:37 +0300

    git reset

--- run.cmd
+++ run.cmd
@@ -1,5 +1,5 @@
-rem git pull https://github.com/SGVp15/ite_course_registration_telegram_bot
 git fetch https://github.com/SGVp15/ite_course_registration_telegram_bot
+git pull https://github.com/SGVp15/ite_course_registration_telegram_bot
 git reset --hard origin/main https://github.com/SGVp15/ite_course_registration_telegram_bot
 
 pip install -r requirements.txt
