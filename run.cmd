db2f916d2f176c36fe62891dfb4028b32720d95c
SGVp15 <phosphorusp15@mail.ru>
2024-10-01 11:39:34 +0300

    git reset

--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
 rem git pull https://github.com/SGVp15/ite_course_registration_telegram_bot
 git reset --hard origin/master
-timeout 3
+timeout 10
 pip install -r requirements.txt
 python main.py
 pause
