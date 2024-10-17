ea3f0ab70e6d7f02392c3e401cc83fb186c91f16
SGVp15 <phosphorusp15@mail.ru>
2024-10-17 16:06:05 +0300

    callback return

--- Telegram/handlers/menu.py
+++ Telegram/handlers/menu.py
@@ -9,7 +9,7 @@ from Telegram.main import dp, bot
 
 @dp.callback_query((F.data == CallBackData.zoom_menu)
                    & (F.from_user.id.in_({*ADMIN_ID, *USERS_ID})))
-async def zoom_menu(callback_query: CallbackQuery):
+async def zoom_menu(callback_query:  types.callback_query):
     await callback_query.message.edit_message_reply_markup(
         chat_id=callback_query.from_user.id,
         message_id=callback_query.message.message_id,
