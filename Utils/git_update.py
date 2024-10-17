adfee88499ec257911dee57e762d2fd872b85c31
SGVp15 <phosphorusp15@mail.ru>
2024-10-17 15:00:17 +0300

    git reset

---
+++ Utils/git_update.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import sys
+from asyncio import sleep
+
+from Utils.log import log
+
+
+def check_for_updates():
+    result = subprocess.run(["git", "pull"], capture_output=True, text=True)
+    rows = str(result.stdout).split('\n')
+    if len(rows) > 3:
+        return True
+    # print(result)
+    # print(bool(result.stdout))
+    return False
+
+
+def pull_updates():
+    subprocess.run(["git", "pull"])
+
+
+def restart_script():
+    python = sys.executable
+    os.execv(python, [python] + sys.argv)
+
+
+async def git_update():
+    while True:
+        if check_for_updates():
+            log.info('BOT UPDATE')
+            pull_updates()
+            restart_script()
+        await sleep(60)  # Проверка каждые 60 секунд
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import asyncio
 from selenium import webdriver
 
 from Telegram.main import start_bot
+from Utils.git_update import git_update
 from Utils.log import log
 from Zoom.Clicker import run_clicker
 
 
 async def main():
     tasks = [
+        git_update(),
         start_bot(),
         run_clicker(),
     ]
