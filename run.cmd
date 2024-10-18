ca3d95305f1ef4fec0db691f06c4bda5c26bc4a4
SGVp15 <phosphorusp15@mail.ru>
2024-10-18 14:41:06 +0300

    bot.edit_message_text

--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
-rem git pull https://github.com/SGVp15/ite_course_registration_telegram_bot
-git reset --hard origin/master
-timeout 10
+git pull
+@REM git reset --hard origin/master
+@REM timeout 10
 pip install -r requirements.txt
 python main.py
 pause
