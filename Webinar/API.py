be1246a560d307257de594a070b9e6e186225803
SGVp15 <phosphorusp15@mail.ru>
2024-11-25 11:34:48 +0300

    api webinar

--- Webinar/API.py
+++ Webinar/API.py
@@ -18,7 +18,7 @@ class WebinarApi:
         Content-Type: application/x-www-form-urlencoded
     };
     """
-
+    base_url = 'https://userapi.webinar.ru/v3'
     def __init__(self, token):
         self.token = token
         self.headers = {'x-auth-token': self.token, 'Content-Type': 'application/x-www-form-urlencoded'}
@@ -30,7 +30,7 @@ class WebinarApi:
     # noinspection PyPep8Naming
     def get_url_client(self, event_sessions_id, events_id: str) -> str:
         # Вывод всех слушателей можно забрать индивидуальные ссылки url
-        request = f'https://userapi.webinar.ru/v3/eventsessions/{event_sessions_id}/participations'
+        request = f'{self.base_url}/eventsessions/{event_sessions_id}/participations'
         base = 'https://events.webinar.ru/ITExpert'
         r = self.get_response(request)
         s = ''
@@ -40,25 +40,27 @@ class WebinarApi:
 
     def get_old_webinars_from_scheduler(self, from_date):
         # Вывод прошедших вебинаров
-        request = f'https://userapi.webinar.ru/v3/stats/events?from={from_date}'
+        request = f'{self.base_url}/stats/events?from={from_date}'
         r = self.get_response(request)
         for row in r:
             log.info('[name] = ', row['eventSessions'][0]['name'])
             log.info('[eventSessionsId] = ', row['eventSessions'][0]['id'])
-            # plog.info(row)
             log.info('')
 
     def get_events_ids_and_names_webinars_from_scheduler(self, from_date: str = None, is_start_webinar=0) -> (dict,
                                                                                                               dict,
                                                                                                               dict):
         # Вывод всех вебинаров можно забрать [eventsessionsID] eventId - для формирования полной ссылки request =
-        # f'https://userapi.webinar.ru/v3/organization/events/schedule?perPage=250&page=1&status[2]=START&from={
+        # f'{base_url}/organization/events/schedule?perPage=250&page=1&status[2]=START&from={
         # from_date}&to=2022-12-30'
+        if from_date is None:
+            now = datetime.now()
+            from_date = now.strftime("%Y-%m-%d+00:00:00")
         status_start = ''
         if is_start_webinar:
             status_start = 'status[2]=START&'
 
-        url = f'https://userapi.webinar.ru/v3/organization/events/schedule?{status_start}'
+        url = f'{self.base_url}/organization/events/schedule?{status_start}'
         if from_date:
             url += f'from={from_date}'
 
@@ -78,7 +80,6 @@ class WebinarApi:
         finally:
             return events_ids, names, description
 
-    # noinspection PyPep8Naming
     def print_link(self, event_sessions_id, event_id):
         s = self.get_url_client(event_sessions_id, event_id)
         rows = s.split('\n')
@@ -86,24 +87,19 @@ class WebinarApi:
         return '\n'.join(rows)
 
     def get_all_registration_url(self) -> str:
-        now = datetime.now()
-        from_date = now.strftime("%Y-%m-%d+00:00:00")
-        is_start_webinar = 0
-
-        events_ids, names, description = self.get_events_ids_and_names_webinars_from_scheduler(from_date,
-                                                                                               is_start_webinar)
-        out = ''
+        events_ids, names, description = self.get_events_ids_and_names_webinars_from_scheduler()
+        out_str = ''
         for eventSessionsID in events_ids:
             event_id = events_ids[eventSessionsID]
-            out += (f'{names[event_id]}\n'
+            out_str += (f'{names[event_id]}\n'
                     f'{self.print_link(eventSessionsID, event_id)}\n')
             if description:
                 try:
-                    out += f'{description[event_id]}\n'
+                    out_str += f'{description[event_id]}\n'
                 except KeyError:
                     pass
-            out += f'{('--' * 70)}\n'
-        return out
+            out_str += f'{('--' * 70)}\n'
+        return out_str
 
     def post_registration_users_list(self, users: list[Contact], send_email_webinar_api='true'):
         # https://events.webinar.ru/event/999146969/1581189808/edit
@@ -112,7 +108,7 @@ class WebinarApi:
         url = ''
         i = 0
         for user in users:
-            url = f'https://userapi.webinar.ru/v3/events/{user.webinar_events_id}/invite'
+            url = f'{self.base_url}/events/{user.webinar_events_id}/invite'
             if (i + 1) % 40 == 0:
                 r = requests.post(url, headers=self.headers, data=data)
                 # [{"participationId":752414983,"email":"g.savushkin@itexpert.ru","link":"https:\/\/my.mts-link.ru\/81296985\/569285096\/7ca38749207b4313ca9c9a420fefcdee"}]
