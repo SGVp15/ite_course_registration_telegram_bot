093036848e911b04fe36905fe2e233e4660ca333
SGVp15 <phosphorusp15@mail.ru>
2024-12-03 11:08:28 +0300

    xml

--- Webinar/sheduler_records.py
+++ Webinar/sheduler_records.py
@@ -5,12 +5,13 @@ from Webinar.config import WEBINAR_TOKENS
 
 
 async def scheduler_record_to_conversions():
-    for token in WEBINAR_TOKENS:
-        webinar_api = WebinarApi(token)
-        records: [dict] = webinar_api.get_records_list()
-        if records:
-            for r in records:
-                record_id = r.get('id')
-                if record_id:
-                    webinar_api.post_record_to_conversions(record_id)
-    await asyncio.sleep(5 * 60)
+    while True:
+        for token in WEBINAR_TOKENS:
+            webinar_api = WebinarApi(token)
+            records: [dict] = webinar_api.get_records_list()
+            if records:
+                for r in records:
+                    record_id = r.get('id')
+                    if record_id:
+                        webinar_api.post_record_to_conversions(record_id)
+        await asyncio.sleep(5 * 60)
