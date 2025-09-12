2a98865f5c1cedb3d38392018d8c909ba0d8793d
SGVp15 <phosphorusp15@mail.ru>
2025-09-12 13:30:08 +0300

    refactoring_ok

--- Telegram/config.py
+++ Telegram/config.py
@@ -5,11 +5,11 @@ from dotenv import dotenv_values, find_dotenv
 config = dotenv_values(find_dotenv('.env'))
 
 BOT_TOKEN = config.get('BOT_TOKEN')
-ADMIN_ID = config.get('ADMIN_ID')
-
+ADMIN_ID = config.get('ADMIN_ID').split(',')
+ADMIN_ID = [w.strip() for w in ADMIN_ID]
 # USERS_ID = list(user_id_email.keys())
-USERS_ID = config.get('USERS_ID')
-
+USERS_ID = config.get('USERS_ID').split(',')
+USERS_ID = [w.strip() for w in USERS_ID]
 DOCUMENTS = os.path.join(os.getcwd(), 'data', 'documents')
 PATH_DOWNLOAD_FILE: str = os.path.join(os.getcwd(), 'data')
 
